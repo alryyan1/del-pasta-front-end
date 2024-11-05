6d550e50085d2d69557bf09190541b116a563847
alryyan <alryyan50@gmail.com>
2024-11-05 19:43:18 +0400

    building

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./app.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 
